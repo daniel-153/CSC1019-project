deb941260b4de4e51db5987082670ed92be2a105
daniel-153 <danieltrin1664@gmail.com>
2025-11-25 01:20:24 -0700

    update parsing to use types

--- parsing.py
+++ parsing.py
@@ -1,17 +1,59 @@
-class JSError(Exception):
-    pass
 
-def getExprSignature(expr):
-    pass
 
-def parseJsExpr(expr):
-    pass
+def getExprSignature(js_str):
+    js_str = js_str.lstrip() # strip leading whitespace
 
-def performJsAssignment(expr):
-    pass
+    # expression is a declaration if it starts with one of these keywords
+    if js_str.startswith(("let ", "var ", "const ")):
+        return JSDeclaration(js_str)
+    
+    js_str = "".join(js_str.split()) # strip all whitespace
 
-def getLineValue(expr):
-    pass
+    # expression is an assignment if it has a single equals sign
+    eq_index = js_str.find("=") 
+    if eq_index != -1 and js_str[eq_index + 1] != "=":
+        return JSAssignment(js_str)
+        
+    # expression is a "simple" expression otherwise
+    return JSExpression(js_str)
+
+def performJSAssignment(assignment, window):
+    var_name = assignment.var_name
+    new_value = assignment.var_value
+    
+    # check if variable has been declared
+    if not var_name in window:
+        raise JSError(f"ReferenceError: {var_name} is not defined.")
+     
+    dec_type = window[var_name][0] # the type of declaration (let, var, or const)
+
+    # if dec_type is const, error, can't assign to a constant variable
+    if dec_type == "const":
+        raise JSError("TypeError: Assignment to constant variable.")
+    
+    window[var_name][1] = new_value
+
+def performJSDeclaration(declaration, window):
+    var_name = declaration.assignment.var_name
+    var_value = declaration.assignment.var_value
+    
+    # check if variable has already been declared
+    if var_name in window:
+        raise JSError(f"SyntaxError: Identifier '{var_name}' has already been declared.")
+    
+    if ( # let and var declarations treated the same
+        declaration.keyword == "let" 
+        or declaration.keyword == "var"
+    ): 
+        if var_value is None: # initialize to undefined
+            window[var_name] = [declaration.keyword, "undefined"]
+        else: # initialize to provided value
+            window[var_name] = [declaration.keyword, var_value]
+    elif declaration.keyword == "const": # const requires a value
+        if var_value is None: # error, const initialization must have a value
+            raise JSError("SyntaxError: Missing initializer in const declaration.")
+        else: # initialize to provided value
+            window[var_name] = [declaration.keyword, var_value]
 
 def eval_js(js_line, window):
     eval_result = {}
@@ -19,12 +61,14 @@ def eval_js(js_line, window):
     try:
         signature = getExprSignature(js_line)
 
-        expr_value = parseJsExpr(signature["expression"])
-
-        if signature["is_assigment"]:
-            performJsAssignment(signature, expr_value, window)
-        
-        eval_result["value"] = getLineValue(signature, expr_value)
+        if isinstance(signature, JSExpression):
+            eval_result["value"] = signature.value
+        elif isinstance(signature, JSAssignment):
+            performJSAssignment(signature, window)
+            eval_result["value"] = signature.var_value
+        elif isinstance(signature, JSDeclaration):
+            performJSDeclaration(signature, window)
+            eval_result["value"] = "undefined"
     except Exception as e:
         eval_result["is_error"] = True
         
@@ -36,4 +80,4 @@ def eval_js(js_line, window):
         if eval_result.get("is_error"):
             return "\u29BB " + str(eval_result.get("error_msg"))
         else:
-            return str(eval_result.get("value"))
+            return eval_result.get("value")
