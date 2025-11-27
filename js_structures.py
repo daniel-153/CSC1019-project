b3ab495cd3e8a656a9f18a8e61092194cc7a103a
daniel-153 <danieltrin1664@gmail.com>
2025-11-27 01:48:39 -0700

    remove validation from constructor

--- js_structures.py
+++ js_structures.py
@@ -7,11 +7,7 @@ class Error(Exception):
         super().__init__(f"{error_type}Error: {description}")
 
 class Variable:
-    def __init__(self, init_type, name, value):
-        # dont allow reserved keywords
-        if name in ['quit', 'NaN', 'true', 'false', 'let', 'var', 'const']:
-            raise Error("Syntax", f"'{name}' is a reserved keyword")
-
-        self.init_type = init_type
+    def __init__(self, dec_keyword, name, value):
+        self.dec_keyword = dec_keyword
         self.name = name
         self.value = value
