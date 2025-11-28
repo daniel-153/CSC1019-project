037d2a0983accd1873b06c95e03d2c9d36e4ddc9
daniel-153 <danieltrin1664@gmail.com>
2025-11-27 21:11:44 -0700

    add types for token detection

--- js_structures.py
+++ js_structures.py
@@ -1,3 +1,5 @@
+import js_types
+
 class Error(Exception):
     def __init__(self, error_type, description):
         self.error_type = error_type
@@ -8,6 +10,92 @@ class Error(Exception):
 
 class Variable:
     def __init__(self, dec_keyword, name, value):
+        # check if the name is valid
+        for idx, char in enumerate(name):
+            if (
+                (idx == 0 and not (char.isalpha() or char == "_")) or
+                not (char.isalpha() or char.isdigit() or char == "_")
+            ): 
+                raise Error("Syntax", f"Invalid identifier '{name}'")
+        
+        if name in ['quit', 'NaN', 'true', 'false', 'let', 'var', 'const', 'undefined', 'null']:
+            raise Error("Syntax", f"'{name}' is a reserved keyword")
+        
         self.dec_keyword = dec_keyword
         self.name = name
         self.value = value
+
+class Token:
+    def __init__(self, tkn_str):
+        self.tkn_str = tkn_str
+
+    class TokenError(Error):
+        def __init__(self, intended_type, tkn_str):
+            super().__init__("Syntax", f"'{tkn_str}' is not a valid {intended_type.__class__.__name__} token")
+
+class Literal(Token):
+    def __init__(self, tkn_str):
+        super().__init__(tkn_str)
+
+        try:
+            self.data_type = js_types.Auto(tkn_str)
+        except js_types.Primitive.ConstructorError:
+            raise self.TokenError(self, tkn_str)
+        
+class Identifier(Token): 
+    def __init__(self, tkn_str):
+        super().__init__(tkn_str)
+
+        try:
+            Variable('let', tkn_str, None)
+        except Error:
+            raise self.TokenError(self, tkn_str)
+
+class Operator(Token):
+    binding_powers = { # for pratt parsing
+        1: {"symbols": ["||"], "bias": 0.1},
+        2: {"symbols": ["&&"], "bias": 0.1},
+        3: {"symbols": ["==", "!=", "===", "!=="], "bias": 0.1},
+        4: {"symbols": [">", "<", ">=", "<="], "bias": 0.1},
+        5: {"symbols": ["+", "-"], "bias": 0.1},
+        6: {"symbols": ["*", "/", "%"]},
+        7: {"symbols": ["**"], "bias": -0.1}
+    }
+
+    def __init__(self, tkn_str):
+        super().__init__(tkn_str)
+        match_found = False
+        match_power = None
+        for power, symbols in self.binding_powers.items():
+            if tkn_str in symbols:
+                match_found = True
+                match_power = power
+                break
+
+        if not match_found:
+            raise self.TokenError(self, tkn_str)
+        
+        self.binding_power = (match_power, match_power + self.binding_powers[match_power]["bias"])
+        
+class Parenthesis(Token): 
+    def __init__(self, tkn_str):
+        super().__init__(tkn_str)
+        if not tkn_str in ["(", ")"]:
+            raise self.TokenError(self, tkn_str)
+        else:
+            self.type = "open" if tkn_str == "(" else "close"
+
+def detectToken(tkn_str):
+    match_found = False
+    for TokenType in [Identifier, Literal, Operator, Parenthesis]:
+        try:
+            matched_tkn = TokenType(tkn_str)
+            match_found = True
+            break
+        except Token.TokenError:
+            pass
+
+    if not match_found:
+        raise Error("Syntax", f"invalid token '{tkn_str}'")
+
+    return matched_tkn
