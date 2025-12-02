a1ed773b57cd42004076e83fdd36c9c36bc5659c
daniel-153 <danieltrin1664@gmail.com>
2025-12-01 22:05:41 -0700

    update examples and add demo video link

--- user_interface.py
+++ user_interface.py
@@ -8,23 +8,26 @@ Only the following structures are supported:
     (1) Declarations (let x;)
     (2) Assignments (z = 3;)
     (3) Expressions (b - 4 / c)
-On each line marked with '>>', you can enter
-a single line of JavaScript code, and the 
-output will be displayed on the next line.
+On each line marked with '[Input JS] >>', you 
+can enter a single line of JavaScript code,
+and the output will be displayed on the next line.
 
 If an entered line is invalid or unsupported,
 an error message will be displayed. You can
-enter 'quit' to end the program.
+enter 'quit' to end the program. Here is a demo video:
+https://drive.google.com/file/d/1ilbcIOeH3gbWDcmLmxzCOoGT57QFZIlD/view?usp=sharing
             
 These are some examples:
->> let x = 5;
-<< 5
->> 2 * (x + 1)
-<< 12
->> const y = x - 1;
-<< 4
->> x === y
-<< false
->> quit
-_____________________________________________"""
+[Input JS] >> let x = 5;
+[ Output ] << 5
+[Input JS] >> 2 * (x + 1)
+[ Output ] << 12
+[Input JS] >> const y = x - 1;
+[ Output ] << 4
+[Input JS] >> x === y
+[ Output ] << false
+[Input JS] >> quit
+
+^ Read Me ^
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"""
     )
